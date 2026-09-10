84ecc98aa28238238719256eecaea5a2a90eafd6
Plziin <slucaslima159@gmail.com>
2026-09-10 17:11:31 -0300

    Add temperature converter in QUESTÃO 3.py

---
+++ QUESTÃO 3.py
@@ -0,0 +1,7 @@
+print ('====== CONVERSOR DE TEMPERATURAS ======')
+celsius = (float(input("Digite uma temperatura em celsius: ")))
+farenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+print (f"Temperatura em celsius = {celsius}°C")
+print (f"Temperatura em farenheit = {farenheit}°F")
+print (f"Temperatura em kelvins = {kelvin}K")
